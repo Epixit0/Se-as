12a98225fed145c5029959d40b48bcf4dcb8f9c8
Leonardo0920 <leo.davila0920@gmail.com>
2024-02-17 20:30:28 -0400

    Se agrego la verificacion que indica si el usurio esta loggeado o no y lo manda a su pantalla correspondiente

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:first_app/firebase_options.dart';
-import 'package:first_app/screens/firstScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+
+import 'auth/auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
         home: Center(
-      child: HomeScreen(),
+      child: Auth(),
     ));
   }
 }
