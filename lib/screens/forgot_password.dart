1f779597fbf99eafd909ad211044a61a9091bb94
Juan Andres <gonzaleznunezjuanandres@gmail.com>
2024-02-21 20:27:15 -0400

    cambios realizados en el apartado de perfil visuales y agregado botom de cambiar contraseña funcional

--- lib/screens/forgot_password.dart
+++ lib/screens/forgot_password.dart
@@ -1,17 +1,146 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/widgets/scaffold_home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class RecuperarContrasena extends StatefulWidget {
-  const RecuperarContrasena({super.key});
+class ChangePasswordScreen extends StatefulWidget {
+  const ChangePasswordScreen({Key? key}) : super(key: key);
 
   @override
-  State<RecuperarContrasena> createState() => _RecuperarContrasenaState();
+  _ChangePasswordScreenState createState() => _ChangePasswordScreenState();
 }
 
-class _RecuperarContrasenaState extends State<RecuperarContrasena> {
+class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
+  final _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore.instance;
+  String _oldPassword = '';
+  String _newPassword = '';
+
   @override
   Widget build(BuildContext context) {
-    return const ScaffoldHome(
-        image: 'assets/images/home.png', child: Text('Olvide Screen'));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cambiar Contraseña'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              onChanged: (value) {
+                _oldPassword = value;
+              },
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Contraseña Antigua',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.lock),
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              onChanged: (value) {
+                _newPassword = value;
+              },
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Nueva Contraseña',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.lock),
+              ),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: () async {
+                await _changePassword();
+              },
+              child: const Text(
+                'Cambiar Contraseña',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+
+  Future<void> _changePassword() async {
+    try {
+      AuthCredential credential = EmailAuthProvider.credential(
+        email: _auth.currentUser!.email!,
+        password: _oldPassword,
+      );
+
+      await _auth.currentUser!.reauthenticateWithCredential(credential);
+
+      await _auth.currentUser!.updatePassword(_newPassword);
+
+      await _updatePasswordInFirestore(_auth.currentUser!.uid, _newPassword);
+
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Contraseña Cambiada'),
+            content: const Text('La contraseña se ha cambiado exitosamente.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context);
+                },
+                child: const Text('Aceptar'),
+              ),
+            ],
+          );
+        },
+      );
+    } catch (e) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Error al Cambiar Contraseña'),
+            content: const Text(
+                'No se pudo cambiar la contraseña. Verifica la contraseña antigua.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Aceptar'),
+              ),
+            ],
+          );
+        },
+      );
+      print(e);
+    }
+  }
+
+  Future<void> _updatePasswordInFirestore(
+      String userId, String newPassword) async {
+    try {
+      await _firestore.collection('users').doc(userId).update({
+        'contrasena': newPassword,
+      });
+    } catch (e) {
+      print('Error updating password in Firestore: $e');
+    }
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: ChangePasswordScreen(),
+  ));
 }
--- lib/screens/perfin.dart
+++ lib/screens/perfin.dart
@@ -1,6 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:first_app/screens/firstScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:first_app/screens/firstScreen.dart';
+import 'forgot_password.dart';
 
 class PerfilPage extends StatefulWidget {
   const PerfilPage({super.key});
@@ -11,75 +13,137 @@ class PerfilPage extends StatefulWidget {
 
 class _PerfilPageState extends State<PerfilPage> {
   final _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore.instance;
+  String _nombre = '';
+  String _correo = '';
+
+  @override
+  void initState() {
+    super.initState();
+    loadUserData();
+  }
+
+  Future<void> loadUserData() async {
+    try {
+      DocumentSnapshot userDoc = await _firestore
+          .collection('users')
+          .doc(_auth.currentUser?.uid)
+          .get();
+
+      _nombre = userDoc['nombre'];
+      _correo = _auth.currentUser?.email ?? '';
+
+      setState(() {});
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [Colors.blue, Colors.red],
+        ),
       ),
-      backgroundColor: const Color.fromARGB(255, 0, 105, 155),
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            height: 200,
-            margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-            padding: const EdgeInsets.all(15),
-            color: const Color.fromARGB(255, 1, 39, 57),
-            child: Center(
-              child: Column(
-                children: [
-                  const Icon(
-                    Icons.account_circle_outlined,
-                    size: 70,
-                    color: Colors.white,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    _auth.currentUser?.email ?? 'Email',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 15,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            width: double.infinity,
-            height: 60,
-            color: const Color.fromARGB(255, 1, 39, 57),
-            padding: const EdgeInsets.all(10),
-            child: GestureDetector(
-              onTap: () {
-                signOut();
-              },
-              child: const Row(
-                children: [
-                  Icon(
-                    Icons.settings_power_outlined,
-                    color: Colors.white,
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    'Cerrar Sesion',
-                    style: TextStyle(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 200,
+                margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 1, 39, 57),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
                         color: Colors.white,
-                        fontSize: 15,
-                        fontWeight: FontWeight.w600),
-                  ),
-                ],
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: CircleAvatar(
+                        radius: 50,
+                        backgroundColor: Colors.white,
+                        child: const Icon(
+                          Icons.account_circle_outlined,
+                          size: 70,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      _nombre,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      _correo,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
+              const SizedBox(height: 20),
+              FloatingActionButton.extended(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ChangePasswordScreen(),
+                    ),
+                  );
+                },
+                label: const Text('Cambiar Contraseña'),
+                icon: const Icon(Icons.lock_open),
+                backgroundColor: Colors.green,
+              ),
+              const SizedBox(height: 20),
+              FloatingActionButton.extended(
+                onPressed: () {
+                  signOut();
+                },
+                label: const Text('Cerrar Sesión'),
+                icon: const Icon(Icons.exit_to_app),
+                backgroundColor: Colors.red,
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
